c4fa125fdc263090f5bf2945faac954a464921de
Igor Zubkov <igor.zubkov@gmail.com>
2010-10-26 13:02:24 +0300

    rails new .

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cucumbertest::Application.config.secret_token = '7ea1de69d015233e17bb83481de32740b1ab09fd0e0f626c04448017821b48ea06fd3b4e787335050d2c1c686d35c636e249994230517ba3d0e6ad7c437b0dd8'
